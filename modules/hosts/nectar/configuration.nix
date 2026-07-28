3d29d47e02add634cace92d98a20ff77ed3ddcf2
Claudio <claudio@dio-olamot.com>
2026-07-28 20:07:33 +0300

    minor fixes

---
+++ modules/hosts/nectar/configuration.nix
@@ -0,0 +1,18 @@
+{ pkgs, ... }:
+
+{
+  # 1. System Hostname & State Version
+  networking.hostName = "nectar";
+
+  # State version for 25.11 release
+  system.stateVersion = "25.11"; 
+
+  # 2. Localization & Timezone
+  time.timeZone = "Asia/Tel_Aviv";
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  # 3. Host-specific Packages
+  environment.systemPackages = with pkgs; [
+    # Host-specific tools can go here
+  ];
+}
