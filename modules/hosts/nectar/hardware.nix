d6a35a6828b58d5fc0ed068e83c1c0bc6a6e90bf
Claudio <claudio@dio-olamot.com>
2026-07-28 21:21:35 +0300

    hardware.nix

--- modules/hosts/nectar/hardware.nix
+++ modules/hosts/nectar/hardware.nix
@@ -1,10 +1,6 @@
 { lib, pkgs, ... }:
 
 {
-  imports = [
-    "${inputs.nixpkgs}/nixos/modules/installer/scan/not-detected.nix"
-  ];
-
   boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
